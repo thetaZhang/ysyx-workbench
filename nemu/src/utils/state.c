6b8290ff778b7be811a2fcf945605d75ce9ad892
tracer-ysyx <tracer@ysyx.org>
2025-06-29 14:42:20 +0800

    > compile NEMU ysyx_250000 zhy Linux dynamic.ipv6.sjtu.edu.cn 6.14.5-200.fc41.x86_64 #1 SMP PREEMPT_DYNAMIC Fri May  2 14:06:21 UTC 2025 x86_64 GNU/Linux  14:42:20 up  2:26,  2 users,  load average: 0.76, 0.68, 0.59

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -18,8 +18,9 @@
 NEMUState nemu_state = { .state = NEMU_STOP };
 
 int is_exit_status_bad() {
-  printf("%d", nemu_state.state);
+  printf("%d\n", nemu_state.state);
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
     (nemu_state.state == NEMU_QUIT);
+  printf("%d\n", good);
   return !good;
 }
