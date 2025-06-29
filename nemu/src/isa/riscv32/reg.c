5303aba960964df251863ea751412d042c04b6ab
tracer-ysyx <tracer@ysyx.org>
2025-06-29 15:11:29 +0800

    > compile NEMU ysyx_250000 zhy Linux dynamic.ipv6.sjtu.edu.cn 6.14.5-200.fc41.x86_64 #1 SMP PREEMPT_DYNAMIC Fri May  2 14:06:21 UTC 2025 x86_64 GNU/Linux  15:11:29 up  2:55,  2 users,  load average: 1.62, 1.19, 0.97

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -24,6 +24,9 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
+  for (int i = 0; i < ARRLEN(regs); i++) {
+    printf("%-15s 0x%08x      %u\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
+  }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
