a3453fc2b7f7f14bf5172b65619109fbe1764e2f
tracer-ysyx <tracer@ysyx.org>
2025-07-03 19:18:39 +0800

    > compile NEMU ysyx_250000 zhy Linux dynamic.ipv6.sjtu.edu.cn 6.14.5-200.fc41.x86_64 #1 SMP PREEMPT_DYNAMIC Fri May  2 14:06:21 UTC 2025 x86_64 GNU/Linux  19:18:39 up  4:32,  2 users,  load average: 0.47, 0.53, 0.53

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -261,6 +261,7 @@ word_t eval(int p, int q, char* e, bool *success){
     switch (tokens[op].type) {
       case '+':{
         res = val1 + val2;
+        printf("val1: %u, val2: %u, res: %u, op: %c\n", val1, val2, res, tokens[op].type);
         return res;
       }
       case '-':{
