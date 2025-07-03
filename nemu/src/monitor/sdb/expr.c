6b543511d08f65e0a6290c23c15e2daa3ff380e3
tracer-ysyx <tracer@ysyx.org>
2025-07-03 20:33:25 +0800

    > compile NEMU ysyx_250000 zhy Linux dynamic.ipv6.sjtu.edu.cn 6.14.5-200.fc41.x86_64 #1 SMP PREEMPT_DYNAMIC Fri May  2 14:06:21 UTC 2025 x86_64 GNU/Linux  20:33:25 up  5:47,  2 users,  load average: 0.37, 0.49, 0.35

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -227,8 +227,12 @@ word_t eval(int p, int q, char* e, bool *success){
       case TK_HEX:
         return strtol(tokens[p].str + 2, NULL, 16);
       case TK_REG: {
-        TODO();
-        break;
+        word_t reg_val = isa_reg_str2val(tokens[p].str + 1, success);
+        if (!*success) {
+          printf("Failed to get register value at position %d: %s\n%s\n%*.s^\n", p, tokens[p].str, e, p, "");
+          return 0;
+        }
+        return reg_val;
       }
       default:{
         *success = false;
