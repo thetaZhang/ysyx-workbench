4290f381dda08326decf9bcf0d397fc78b1740e2
tracer-ysyx <tracer@ysyx.org>
2025-08-27 21:25:50 +0800

    > compile NEMU ysyx_250000 zhy Linux dynamic.ipv6.sjtu.edu.cn 6.15.10-100.fc41.x86_64 #1 SMP PREEMPT_DYNAMIC Fri Aug 15 14:55:12 UTC 2025 x86_64 GNU/Linux  21:25:50 up  7:50,  2 users,  load average: 0.77, 0.74, 0.80

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -246,6 +246,7 @@ word_t eval(int p, int q, char* e, bool *success){
         return strtol(tokens[p].str + 2, NULL, 16);
       case TK_REG: {
         word_t reg_val = 0;
+        printf("reg token: %s\n", tokens[p].str);
         if (strcmp(tokens[p].str, "$0")) {
           reg_val = isa_reg_str2val(tokens[p].str, success);
         } 
