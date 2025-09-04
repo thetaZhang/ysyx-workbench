f5d49db63532cd379a2e8a655821dbae1c2434f4
zhy-ubuntu-vm <Zhy_theta@outlook.com>
2025-09-04 21:54:17 +0800

    NEMU support RV32IM

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -44,6 +44,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   if (wp_difftest()) {
     nemu_state.state = NEMU_STOP;
+    printf("Hit watchpoint at pc = " FMT_WORD "\n", _this->pc);
   }
 }
 
