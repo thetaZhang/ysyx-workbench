eac4f0e6f680ab3eab01186b2ba7376dace70d3c
tracer-ysyx <tracer@ysyx.org>
2025-09-05 20:40:16 +0800

    > compile NEMU ysyx_250000 zhy Linux zhy-Ubuntu22 6.8.0-78-generic #78~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 13 14:32:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:40:16 up  4:49,  1 user,  load average: 0.01, 0.02, 0.00

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -58,7 +58,7 @@ char *strcat(char *dst, const char *src) {
 
 int strcmp(const char *s1, const char *s2) {
   size_t i = 0;
-  while (s1[i] != '\0' && s1[i] != s2[i]) {
+  while (s1[i] != '\0' && s1[i] == s2[i]) {
     i++;
   }
   return ((unsigned char)s1[i] - (unsigned char)s2[i]);
@@ -66,7 +66,7 @@ int strcmp(const char *s1, const char *s2) {
 
 int strncmp(const char *s1, const char *s2, size_t n) {
   size_t i = 0;
-  while (s1[i] != '\0' && s1[i] != s2[i] && i < n) {
+  while (s1[i] != '\0' && s1[i] == s2[i] && i < n) {
     i++;
   }
   if (i == n) return 0;
