abbc20e70cf1f63b7ade94b1fd9091390ce37a25
tracer-ysyx <tracer@ysyx.org>
2025-09-05 21:28:59 +0800

    > compile NEMU ysyx_250000 zhy Linux zhy-Ubuntu22 6.8.0-78-generic #78~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 13 14:32:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:28:59 up  5:38,  1 user,  load average: 0.01, 0.07, 0.06

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -12,7 +12,6 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER; // defined in CFLAGS
 
 void putch(char ch) {
-  puts(&ch, 1);
 }
 
 void halt(int code) {
