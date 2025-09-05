5ac428b6cf3d73b2639bda0d27d6b674df1da678
tracer-ysyx <tracer@ysyx.org>
2025-09-05 20:31:38 +0800

    > compile NEMU ysyx_250000 zhy Linux zhy-Ubuntu22 6.8.0-78-generic #78~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 13 14:32:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:31:38 up  4:41,  1 user,  load average: 0.00, 0.02, 0.00

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -57,27 +57,63 @@ char *strcat(char *dst, const char *src) {
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  size_t i = 0;
+  while (s1[i] != '\0' && s1[i] != s2[i]) {
+    i++;
+  }
+  return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  size_t i = 0;
+  while (s1[i] != '\0' && s1[i] != s2[i] && i < n) {
+    i++;
+  }
+  if (i == n) return 0;
+  return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  for (size_t i = 0;i < n; i++) {
+    ((unsigned char *)s)[i] = (unsigned char)c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  if (dst == src || n == 0) {
+    return dst;
+  }
+  else if (dst < src){
+    for (size_t i = 0; i < n; i++) {
+      ((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+    }
+  }
+  else{
+    for (size_t i = n; i > 0; i--) {
+      ((unsigned char *)dst)[i - 1] = ((unsigned char *)src)[i - 1];
+    }
+  }
+
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  for (size_t i = 0; i < n; i++) {
+    ((unsigned char *)out)[i] = ((unsigned char *)in)[i];
+  }
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  size_t i = 0;
+  while (i < n) {
+    if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i]) {
+      return ((unsigned char *)s1)[i] - ((unsigned char *)s2)[i];
+    }
+    i++;
+  }
+  return 0;
 }
 
 #endif
