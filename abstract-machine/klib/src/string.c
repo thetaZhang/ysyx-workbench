2825ecfee7311fcf78b9ea191f1015eb6ac392ae
tracer-ysyx <tracer@ysyx.org>
2025-09-05 20:06:54 +0800

    > compile NEMU ysyx_250000 zhy Linux zhy-Ubuntu22 6.8.0-78-generic #78~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 13 14:32:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:06:54 up  4:16,  1 user,  load average: 0.03, 0.06, 0.04

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,19 +5,55 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  if (s == NULL) {
+    return 0;
+  }
+  
+  size_t len = 0;
+  while(s[len] != '\0') {
+    len++;
+  }
+  return len;
+  
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  if (dst == NULL || src == NULL) {
+    return NULL;
+  }
+
+  char *ret = dst;
+  while (*src != '\0'){
+    *dst = *src;
+    dst++;
+    src++;
+  }
+  *dst = '\0';
+  return ret;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  if (dst == NULL || src == NULL) {
+    return NULL;
+  }
+
+  char *ret = dst;
+  size_t i;
+  for (i = 0; i < n && src[i] != '\0'; i++) {
+    dst[i] = src[i];
+  }
+    
+  for (; i < n; i++) {
+    dst[i] = '\0';
+  }
+  return ret;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  size_t len_dst= strlen(dst);
+  strcpy(dst + len_dst, src);
+  return dst;
+
 }
 
 int strcmp(const char *s1, const char *s2) {
