5d278d18f3b810fcb275f5f196461113b481139a
tracer-ysyx <tracer@ysyx.org>
2025-09-10 18:51:46 +0800

    > sim RTL ysyx_250000 zhy Linux zhy-Ubuntu22 6.8.0-79-generic #79~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 15 16:54:53 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:51:46 up  1:39,  2 users,  load average: 0.21, 0.10, 0.03

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,20 +1,29 @@
 #include "Vtop.h"
 #include "verilated.h"
+#include "verilated_fst_c.h"
 #include <stdio.h>
 #include <assert.h>
 
 int main(int argc, char** argv){
 	VerilatedContext* contextp = new VerilatedContext;	 contextp->commandArgs(argc, argv);
   Vtop* top = new Vtop{contextp};
+  Verilated::traceEverOn(true);
+  VerilatedFstC* tfp = new VerilatedFstC;
+  top->trace(tfp, 0);
+  tfp->open("build/waveform.fst");
+  uint64_t main_time = 0;
   while (!contextp->gotFinish()) { 
     int a = rand() & 1;
     int b = rand() & 1;
     top->a = a;
     top->b = b;
     top->eval();
+    tfp->dump(main_time++);
     printf("a=%d b=%d f=%d\n", a, b, top->f);
     assert(top->f == (a ^ b));
+    if (main_time > 50) break;
   }
+  tfp->close();
   delete top;
   delete contextp;
   return 0;
