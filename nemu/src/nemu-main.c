84cfcf9747be38d267a6f6841770fba74d5a5986
tracer-ysyx <tracer@ysyx.org>
2025-09-21 19:56:04 +0800

    > compile NEMU ysyx_250000 zhy Linux zhy-Ubuntu22 6.8.0-83-generic #83~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 18:19:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:56:04 up  3:25,  2 users,  load average: 0.25, 0.05, 0.02

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -23,8 +23,10 @@ int is_exit_status_bad();
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
+  printf("Initializing AM monitor...\n");
   am_init_monitor();
 #else
+  printf("Initializing default monitor...\n");
   init_monitor(argc, argv);
 #endif
 
