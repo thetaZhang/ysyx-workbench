a438eb33b2b31b472fea373cc9800e230fc58b4d
tracer-ysyx <tracer@ysyx.org>
2025-09-21 20:25:34 +0800

    > compile NEMU ysyx_250000 zhy Linux zhy-Ubuntu22 6.8.0-83-generic #83~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 18:19:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:25:34 up  3:54,  1 user,  load average: 0.05, 0.09, 0.06

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -53,7 +53,7 @@ void init_map() {
 }
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
-  printf("addr %08x\n", addr);
+  printf("addr %08x, low %08x\n", addr, map->low);
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
