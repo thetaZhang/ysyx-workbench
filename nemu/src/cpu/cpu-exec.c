8dbfea1de179821117b881daeebc1a9dc03ce350
tracer-ysyx <tracer@ysyx.org>
2025-09-22 11:47:02 +0800

    > compile NEMU ysyx_250000 zhy Linux zhy-Ubuntu22 6.8.0-83-generic #83~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 18:19:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:47:02 up 56 min,  2 users,  load average: 0.08, 0.07, 0.03

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -42,10 +42,13 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+
+#ifdef CONFIG_WATCHPOINT
   if (wp_difftest()) {
     nemu_state.state = NEMU_STOP;
     printf("Hit watchpoint at pc = " FMT_WORD "\n", _this->pc);
   }
+#endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
