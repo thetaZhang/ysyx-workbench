7e6e0f59038679ad159e61d6b7ca9943fec6e493
tracer-ysyx <tracer@ysyx.org>
2025-09-23 21:55:50 +0800

    > compile NEMU ysyx_250000 zhy Linux zhy-Ubuntu22 6.8.0-83-generic #83~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 18:19:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:55:50 up  7:42,  1 user,  load average: 0.25, 0.12, 0.08

--- nemu/include/memory/paddr.h
+++ nemu/include/memory/paddr.h
@@ -28,7 +28,13 @@ uint8_t* guest_to_host(paddr_t paddr);
 paddr_t host_to_guest(uint8_t *haddr);
 
 static inline bool in_pmem(paddr_t addr) {
-  return addr - CONFIG_MBASE < CONFIG_MSIZE;
+  //return addr - CONFIG_MBASE < CONFIG_MSIZE;
+  bool ret = addr - CONFIG_MBASE < CONFIG_MSIZE;
+
+  if (!ret) {
+    printf("addr = 0x%08x\n", addr);
+  }
+  return ret;
 }
 
 word_t paddr_read(paddr_t addr, int len);
